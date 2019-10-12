334580009b1512e7d7d8b3ff6a1aed89fca20ed2
Hamid Behnam <hamid.behnam@gmail.com>
2019-10-12 15:17:49 -0700

    add top level reducer and effect and registering them in app module

---
+++ src/app/app.reducer.ts
@@ -0,0 +1,29 @@
+import {ActionReducerMap} from '@ngrx/store';
+
+export interface State {
+
+}
+
+export const reducer: ActionReducerMap<State> = {};
+
+
+// import { Action } from '@ngrx/store';
+//
+//
+// export const appFeatureKey = 'app';
+//
+// export interface State {
+//
+// }
+//
+// export const initialState: State = {
+//
+// };
+//
+// export function reducer(state = initialState, action: Action): State {
+//   switch (action.type) {
+//
+//     default:
+//       return state;
+//   }
+// }
